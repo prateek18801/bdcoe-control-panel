3d5b23a565d5346296e32d323570ea124e3cb0b5
Prateek Chaurasia <prateek18801@gmail.com>
2022-10-28 23:21:51 +0530

    add member schema

---
+++ models/member.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose');
+
+const memberSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    stdno: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    contact: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    branch: {
+        type: String,
+        enum: ['CSE', 'CS', 'CSE(DS)', 'CSE(AIML)', 'AIML', 'IT', 'CSIT', 'ECE', 'EN', 'ME', 'CIVIL', 'OTHER'],
+        required: true
+    },
+    section: {
+        type: Number,
+        required: true
+    },
+    graduation: {
+        type: Number,
+        required: true
+    },
+    coordinator: {
+        type: Boolean,
+        required: true
+    },
+    linkedin: {
+        type: String,
+        unique: true
+    },
+    github: {
+        type: String,
+        unique: true
+    },
+    imageUrl: {
+        type: String,
+        unique: true
+    },
+    website: {
+        type: String,
+        unique: true
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model('Member', memberSchema);
