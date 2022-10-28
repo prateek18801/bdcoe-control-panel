5be13cb8a5117dd32fa6946559114fa90abcb9c1
Prateek Chaurasia <prateek18801@gmail.com>
2022-10-28 23:24:49 +0530

    add admin routes and db

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 
+require('./utils/db').connect();
+const adminRouter = require('./routes/admin');
+
 const app = express();
 
-app.use('/', (req, res, next) => {
-    res.status(200).send('hello');
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('/', (req, res, next) => {
+    res.status(200).send('server running');
+});
+
+app.use('/admin', adminRouter);
+
+app.use((err, req, res, next) => {
+    res.status(500).json({message: err.message});
 });
 
 app.listen(process.env.PORT, ()=>{
