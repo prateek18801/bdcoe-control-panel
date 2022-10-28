c4ce7b59c652ba178f3c9876fbb24d221218da2b
Prateek Chaurasia <prateek18801@gmail.com>
2022-10-28 23:23:23 +0530

    add member registration routes

---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const adminController = require('../controllers/admin');
+
+router.get('/member/register', adminController.getMemberRegistration);
+
+router.post('/member/register', adminController.postMemberRegistration);
+
+module.exports = router;
