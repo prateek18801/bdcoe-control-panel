95080283a346f34639b68e0bfdb87952921cd25a
Prateek Chaurasia <prateek18801@gmail.com>
2022-10-29 17:49:21 +0530

    add get status route

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const apiController = require('../controllers/api');
 
+router.get('/v1/status', apiController.getStatus);
+
 router.get('/v1/event/:code?', apiController.getEvent);
 
 router.get('/v1/member/:id?', apiController.getMember);
