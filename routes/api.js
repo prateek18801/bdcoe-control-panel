9efb36b18cee7206008e7f9815acab51ada27742
Prateek Chaurasia <prateek18801@gmail.com>
2022-10-29 16:31:57 +0530

    add member event api routes

---
+++ routes/api.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const apiController = require('../controllers/api');
+
+router.get('/v1/event/:code?', apiController.getEvent);
+
+router.get('/v1/member/:id?', apiController.getMember);
+
+module.exports = router;
