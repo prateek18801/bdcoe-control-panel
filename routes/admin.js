32f79a5cf2bfd48b4c3babcd667827994164dad1
Prateek Chaurasia <prateek18801@gmail.com>
2022-10-31 15:32:10 +0530

    rename routes restructure

--- routes/admin.js
+++ routes/admin.js
@@ -2,16 +2,30 @@ const router = require('express').Router();
 
 const adminController = require('../controllers/admin');
 
+// PAGES
+
+router.get('/dashboard', adminController.getDashboard);
+
+router.get('/v/member/:id?', adminController.getMember);
+
+// router.get('/v/event/:code?', adminController.getEvent);
+
+router.get('/r/member', adminController.getMemberRegistration);
+
+// API
+
+router.get('/toggle', adminController.toggleRegistration);
+
 router.post('/config', adminController.postConfig);
 
-router.get('/toggle-registration', adminController.toggleRegistration);
-
-router.get('/member/:id?', adminController.getMember);
-
-router.get('/member/register', adminController.getMemberRegistration);
-
-router.post('/member/register', adminController.postMemberRegistration);
+router.post('/r/member', adminController.postMemberRegistration);
 
 router.post('/event', adminController.postEvent);
+
+router.get('/d/registration', adminController.downloadRegistrationData);
+
+router.get('/d/event', adminController.downloadEventData)
+
+router.get('/d/member', adminController.downloadMemberData)
 
 module.exports = router;
