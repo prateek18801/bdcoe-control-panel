b070c3a29918d88ea86f3ab1186cea244c9966fc
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-06 12:41:20 +0530

    add jwt set cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
@@ -17,10 +19,21 @@ exports.getResetPassword = (req, res, next) => {
 exports.postLogin = async (req, res, next) => {
     const { username, password } = req.body;
     try {
-        const found = await User.findOne({username});
-        if(found) {
-            // compare password
-            // if match redirect dashboard give jwt
+        // const found = await User.findOne({username});
+        const found = {
+            _id: 'prateek18801',
+            password: 'password'
+        }
+        if (found) {
+            if (password === found.password) {
+                console.log(process.env.JWT_TOKEN);
+                const token = jwt.sign({ id: found._id }, process.env.JWT_SECRET, { expiresIn: '3d' });
+                console.log(token);
+                return res.cookie('auth', token, {
+                    httpOnly: true,
+                    maxAge: 259200000
+                }).status(300).redirect('/admin/dashboard');
+            }
         }
         return res.status(401).redirect(`/auth/login?u=${username}`);
     } catch (err) {
