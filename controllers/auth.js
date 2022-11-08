a7b2fc75c4387024e162eb393754da5672030171
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-08 10:51:47 +0530

    add update password controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,9 +55,9 @@ exports.postSignup = async (req, res, next) => {
     const { username, password, email, role } = req.body;
     try {
         if (password.length < 8) {
-            const error = new Error('password should be min. 8 characters long');
-            error.code = 400;
-            throw error;
+            return res.status(400).json({
+                message: 'falied'
+            });
         }
         const hash = await bcrypt.hash(password, 10);
         const saved = await new User({ password: hash, username, email, role }).save();
@@ -68,6 +68,32 @@ exports.postSignup = async (req, res, next) => {
                 email: saved.email,
                 role: saved.role
             }
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+exports.postUpdatePassword = async (req, res, next) => {
+    const { password, new_password, conf_password} = req.body;
+    try {
+        if(new_password !== conf_password || new_password.length < 8) {
+            return res.status(400).json({
+                message: 'falied',
+            });
+        }
+        const user = await User.findOne({ username: req.user.username });
+        const match = await bcrypt.compare(password, user.password);
+        if (!match) {
+            return res.status(403).json({
+                message: 'forbidden'
+            });
+        }
+        const hash = await bcrypt.hash(new_password, 10);
+        user.password = hash;
+        await user.save();
+        return res.status(200).json({
+            message: 'updated'
         });
     } catch (err) {
         next(err);
