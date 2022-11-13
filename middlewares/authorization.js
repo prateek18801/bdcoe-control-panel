4b31c1cd0aeb472978039452a7db0e8a2c5c1919
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-13 11:25:01 +0530

    add login redirect

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,8 +1,11 @@
 const roles = ['admin', 'member', 'user'];
 
-const authorization = (role)  => {
+const authorization = (role) => {
     return (req, res, next) => {
-        if(roles.indexOf(req.user.role) > roles.indexOf(role)) {
+        if (!req.user) {
+            return res.status(401).redirect('/auth/login');
+        }
+        if (roles.indexOf(req.user.role) > roles.indexOf(role)) {
             return res.status(403).json({
                 message: 'forbidden'
             });
