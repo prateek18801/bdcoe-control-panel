3bf941adf7b5e2e4540339b98a5403328cb97568
Prateek Chaurasia <prateek18801@gmail.com>
2022-11-13 11:24:16 +0530

    add logout route

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,8 @@ router.get('/login', authController.getLogin);
 
 router.post('/login', authController.postLogin);
 
+router.get('/logout', authController.getLogout);
+
 router.get('/signup', authController.getSignup);
 
 router.post('/signup', authController.postSignup);
