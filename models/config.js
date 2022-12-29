8fcb7c8f3687434780cae21c27efe7a3237e7d8e
Prateek Chaurasia <prateek18801@gmail.com>
2022-12-29 23:19:20 +0530

    refactor model change field names

--- models/config.js
+++ models/config.js
@@ -2,15 +2,20 @@ const mongoose = require('mongoose');
 
 const configSchema = new mongoose.Schema({
     upcoming_event: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Event'
+        type: String,
+        required: true
     },
-    registration: {
+    event_code: {
+        type: String,
+        required: true
+    },
+    form_status : {
         type: Boolean,
         default: false
     },
-    modified: {
-        type: Date
+    modified_by: {
+        type: String,
+        required: true
     }
 }, {timestamps: true});
 
