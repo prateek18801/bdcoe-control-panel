019dc28cb3b162d91240f2e13e985fb9889c20bb
Prateek Chaurasia <prateek18801@gmail.com>
2023-01-01 23:59:41 +0530

    add contact confirmation module

--- services/email.js
+++ services/email.js
@@ -2,13 +2,25 @@ const sendgrid = require('@sendgrid/mail');
 
 sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.sendConfirmationEmail = (name, email) => {
+exports.registerEmail = (name, email) => {
 
     const message = {
         to: `${name} <${email}>`,
         from: 'Big Data Centre of Excellence <bdcoe@akgec.ac.in>',
         subject: 'Registration Successful',
         text: 'You have successfully resigtered for the event'
+    }
+
+    deliver(message);
+}
+
+exports.contactEmail = (name, email) => {
+
+    const message = {
+        to: `${name} <${email}>`,
+        from: 'Big Data Centre of Excellence <bdcoe@akgec.ac.in>',
+        subject: 'Thank you for contacting us',
+        text: 'Thanks for getting in touch, This is an automatic response to let you know that we\'ve received your message and are working on the same. We\'ll reach out to you as soon as possible.'
     }
 
     deliver(message);
